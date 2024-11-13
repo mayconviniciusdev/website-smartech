c0dccdc6ee581d33516094cf38c435deefa12410
Maycon Vinicius <contato@mayconviniciusdev.com>
2024-11-13 15:31:52 -0300

    Updating company pillars

--- app/content/about/aboutPillars.ts
+++ app/content/about/aboutPillars.ts
@@ -4,6 +4,12 @@ const pillars = [
   {
     icon: faEye,
     title: "VISSÃO",
+    description: "Ser referência de excelência e qualidade em produtos e serviços para informática, e um dos principais fornecedores do setor através de um portfólio de qualidade com características únicas e eficientes.",
+  },
+
+  {
+    icon: faHandshakeAngle,
+    title: "VALORES",
     description: "Nosso relacionamento de excelência com nossos clientes e colaboradores devem ser de forma clara e transparente, baseado em relações de confiança, ética, habilidade de comunicação, respeito, motivação, integridade e sabedoria.",
   },
 
@@ -11,12 +17,6 @@ const pillars = [
     icon: faCompass,
     title: "MISSÃO",
     description: "Proporcionar aos nossos clientes a mais alta tecnologia e qualidade como assistência técnica, oferecendo soluções definitivas e garantir a excelência na entrega de produtos e serviços, agregando valor aos nossos clientes.",
-  },
-
-  {
-    icon: faHandshakeAngle,
-    title: "VALORES",
-    description: "Nosso relacionamento de excelência com nossos clientes e colaboradores devem ser de forma clara e transparente, baseado em relações de confiança, ética, habilidade de comunicação, respeito, motivação, integridade e sabedoria.",
   },
 ];
 
