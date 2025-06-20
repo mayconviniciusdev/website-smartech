586dda2abf76d45521e6db680538f633e5fa32dd
Maycon Vinicius <contato@mayconviniciusdev.com>
2025-06-19 21:47:48 -0300

    chore: refactor Tailwind config with explicit typing and updated content paths

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,22 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./styles/**/*.{js,ts,jsx,tsx,css}",
   ],
   theme: {
     extend: {
       colors: {
-        'white': "#F6F6F6",
-        'blue': '#052B47',
-        'blue-white': '#D6EDFF',
-        'blue-cyan': '#0E7178',
-        'black-grey': '#1E1E1E',
+        white: "#F6F6F6",
+        blue: '#052B47',
+        "blue-white": '#D6EDFF',
+        "blue-cyan": '#0E7178',
+        "black-grey": '#1E1E1E',
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
+
+export default config;
