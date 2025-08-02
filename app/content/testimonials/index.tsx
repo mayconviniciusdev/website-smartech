936e4d11a62570ef653e2e55ed0dc7299c3a71a2
Maycon Vinicius <contato@mayconviniciusdev.com>
2025-08-02 14:47:00 -0300

    style: increase section max width to 6xl and adjust spacing

--- app/content/testimonials/index.tsx
+++ app/content/testimonials/index.tsx
@@ -10,20 +10,20 @@ import { faQuoteLeft } from "@fortawesome/free-solid-svg-icons";
 
 export const Testimonials = () => {
   return (
-    <section className="max-w-4xl mx-auto text-center">
-      <div className="mx-5">
-        <h2 className="font-bold text-2xl text-blue">O QUE DIZEM SOBRE A GENTE?</h2>
+    <section className="max-w-6xl mx-auto text-center">
+      <div className="mx-5 mt-20 mb-10">
+        <h2 className="font-bold text-2xl text-blue text-start">O QUE DIZEM SOBRE A GENTE?</h2>
 
-        <div className="mt-8">
+        <div className="mb-5">
           <Swiper
-          breakpoints={{ 0: { slidesPerView: 1 }, 768: {slidesPerView: 2}, 1024: { slidesPerView: 2 }}}
+          breakpoints={{ 0: { slidesPerView: 1 }, 768: {slidesPerView: 2}, 1024: { slidesPerView: 3 }}}
           spaceBetween={20}
           modules={[Autoplay, Pagination, A11y]}
           pagination={{ clickable: true, el: ".custom-pagination" }}
           autoplay={{ delay: 2500, disableOnInteraction: false }}
           loop={true}>
             {testimonials.map((testemony, index) => (
-              <SwiperSlide key={index} className="items-center">
+              <SwiperSlide key={index} className="items-center mt-4">
                 <div className="bg-blue/20 p-10 rounded-2xl">
                   <FontAwesomeIcon icon={faQuoteLeft} className="text-6xl text-blue"/>
                   <h2 className="font-bold text-lg text-black-grey py-2">{testemony.name}</h2>
