8ce221fb8ae29eabd2f5745984fd1a8d88c42bbb
Maycon Vinicius <contato@mayconviniciusdev.com>
2025-08-02 14:13:54 -0300

    refactor: move About banner to Header, increase visible events to 5, adjust spacing and scroll offset

--- app/content/about/index.tsx
+++ app/content/about/index.tsx
@@ -1,30 +1,22 @@
-import { Element, Link } from "react-scroll";
+import { useState } from "react";
+import { Element } from "react-scroll";
 
 import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 
 import timelineEvents from "../../data/aboutTimeline";
+import pillars from "../../data/aboutPillars";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useEffect, useState } from "react";
-import pillars from "../../data/aboutPillars";
 
 export const About = () => {
   const [showMore, setShowMore] = useState(false);
   const [visibleEvents, setVisibleEvents] = useState(2);
-  const [isSafari, setIsSafari] = useState(false);
-
-  useEffect(() => {
-    const ua = navigator.userAgent;
-    const safari = /^((?!chrome|android).)*safari/i.test(ua);
-    setIsSafari(safari);
-  }, []);
-
-
+  
   const toggleShowMore = () => {
     setShowMore((prevState) => {
       const newShowMore = !prevState;
-      if (newShowMore) { setVisibleEvents((prevEvents) => prevEvents + 3); } 
+      if (newShowMore) { setVisibleEvents((prevEvents) => prevEvents + 5); } 
       else { setVisibleEvents(2); }
       return newShowMore;
     });
@@ -32,36 +24,8 @@ export const About = () => {
 
   return (
     <section>
-      <div className="relative h-[700px]">
-        <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white">
-          <p className="text-3xl font-bold mb-2">SOMOS<br/>ESPECIALIZADOS EM</p>
-          <p className="text-2xl font-medium">manutenção de computadores,<br/> notebooks e celulares.</p>
-
-          <Link 
-          smooth={true}
-          duration={500}
-          to="contact"
-          className="mt-8 px-6 py-3 bg-blue rounded-xl cursor-pointer font-medium hover:text-blue-white">
-            FAÇA UM ORÇAMENTO
-          </Link>
-        </div>
-
-        {isSafari ? 
-        (<img
-        src="images/bg-smartech.mp4"
-        alt="Video poster"
-        className="w-[100%] h-full object-cover"/>) 
-        : (<video
-          src="images/bg-smartech.mp4"
-          className="w-[100%] h-full object-cover"
-          muted
-          autoPlay
-          playsInline
-          loop/>)}
-      </div>
-
       <Element name="about">
-        <div className="max-w-6xl mx-auto py-10">
+        <div className="max-w-6xl mx-auto">
           <div className="mx-5">
             <h2 className="font-bold text-2xl text-blue mb-5">SOBRE NÓS</h2>
             <VerticalTimeline className="custom-timeline">
@@ -79,7 +43,7 @@ export const About = () => {
               ))}
             </VerticalTimeline>
 
-            <div className="text-center mt-5">
+            <div className="text-center my-6">
               <button
               onClick={toggleShowMore}
               className="text-blue font-bold">
@@ -90,7 +54,7 @@ export const About = () => {
         </div>
 
         <div className="max-w-6xl mx-auto">
-          <div className="mx-5 items-center grid lg:grid-cols-3 gap-10">
+          <div className="mx-5 items-center grid lg:grid-cols-3 gap-10 mt-10">
             {pillars.map((item, index) => (
               <div key={index} className="text-center p-8 bg-blue/20 shadow-2xl shadow-blue-cyan/50 rounded-2xl">
                 <FontAwesomeIcon icon={item.icon} className="text-blue text-3xl"/>
@@ -102,6 +66,5 @@ export const About = () => {
         </div>
       </Element>
     </section>
-    
   )
 }
--- app/content/header/index.tsx
+++ app/content/header/index.tsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Element, Link } from "react-scroll";
+
 import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Element, Link } from "react-scroll";
 
 interface MenuItem { label: string, to: string; }
 const menuItems: MenuItem[] = [
@@ -15,9 +16,16 @@ export const Headers = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const toggleMenu = () => {setMenuOpen(!menuOpen);};
 
+  const [isSafari, setIsSafari] = useState(false);
+  useEffect(() => {
+    const ua = navigator.userAgent;
+    const safari = /^((?!chrome|android).)*safari/i.test(ua);
+    setIsSafari(safari);
+  }, []);
+
   return (
     <Element name="initial">
-      <section className="py-10 mx-5">
+      <div className="my-10 mx-5">
         {/* desktop menu */}
         <div className="flex justify-end md:justify-center">  
           <div className="bg-blue/25 mr-10 p-2 rounded-2xl hidden lg:flex justify-center">
@@ -31,6 +39,7 @@ export const Headers = () => {
                   <Link 
                   to={item.to}
                   smooth={true}
+                  offset={-50}
                   duration={500}
                   className="cursor-pointer hover:text-black-grey">
                     {item.label}
@@ -62,6 +71,7 @@ export const Headers = () => {
                     to={item.to}
                     smooth={true}
                     duration={500}
+                    offset={-50}
                     className="flex flex-col items-end cursor-pointer hover:text-black-grey my-2">
                       {item.label}
                     </Link>
@@ -70,8 +80,38 @@ export const Headers = () => {
               </ul> 
             </nav>
           )}
+        </div>    
+      </div>
+
+      {/* banner */}
+      <div className="relative h-[700px]">
+        <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white z-10">
+          <p className="text-3xl font-bold mb-2">SOMOS<br/>ESPECIALIZADOS EM</p>
+          <p className="text-2xl font-medium">manutenção de computadores,<br/> notebooks e celulares.</p>
+
+          <Link 
+          smooth={true}
+          duration={500}
+          to="contact"
+          offset={-50}
+          className="mt-8 px-6 py-3 bg-blue rounded-xl cursor-pointer font-medium hover:text-blue-white">
+            FAÇA UM ORÇAMENTO
+          </Link>
         </div>
-      </section>
+
+        {isSafari ? 
+        (<img
+        src="images/bg-smartech.mp4"
+        alt="Video poster"
+        className="w-[100%] h-full object-cover z-0"/>) 
+        : (<video
+          src="images/bg-smartech.mp4"
+          className="w-[100%] h-full object-cover z-0"
+          muted
+          autoPlay
+          playsInline
+          loop/>)}
+      </div>
     </Element>
   )
 }
