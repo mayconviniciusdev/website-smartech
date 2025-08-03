8d3ed9d4b111322925567df6cc1a9dea7eddb24d
Maycon Vinicius <contato@mayconviniciusdev.com>
2025-08-03 17:18:14 -0300

    chore: configura next.config.js com trailingSlash e output export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: false,
+  trailingSlash: true,
+  output: 'export',
 };
 
 export default nextConfig;
