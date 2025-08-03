163a44c31bdd2a41e8a744bdc36d68b0f2e398dc
Maycon Vinicius <contato@mayconviniciusdev.com>
2025-08-03 20:07:06 -0300

    chore: move testimonials section files to components/sections/testimonials

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { Headers } from "./components/sections/header";
 import { About } from "./components/sections/about";
 import { Services } from "./components/sections/services";
 import { Portfolio } from "./components/sections/portfolio";
-import { Testimonials } from "./content/testimonials";
+import { Testimonials } from "./components/sections/testimonials";
 import { Footer } from "./components/sections/footer";
 
 const PageContent = () => {
